c763ef18f956de5e62a3edf3c0bd62c82e3c0eb1
Teja-Gedi <tejai9347@gmail.com>
2023-03-18 17:27:43 +0530

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function display(){
+    for(var t=4; t<=10; t++){
+        document.write(t+ " ");
+    }
+
+}
+display();
+ 
+const student = {name: " teja"};
+student ={name:" teja", age:24}
+ 
+document.write(student.name);
